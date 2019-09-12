4f1c2fa1a5b0b8da2e0c68c44e773261dd725e91
Ivan Titkov <ivan-1985@tut.by>
2019-09-12 06:14:05 +0300

    content complete

---
+++ Views/ContentEditView.xaml.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace IShop_Management.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для ContentEditView.xaml
+    /// </summary>
+    public partial class ContentEditView : Window
+    {
+        string ProdId;
+        string ProdName;
+        string ProdCat;
+        string ProdDescr;
+        public ContentEditView(string ProdId, string ProdName, string ProdCat, string ProdDescr)
+        {
+            this.ProdId = ProdId;
+            this.ProdName = ProdName;
+            this.ProdCat = ProdCat;
+            this.ProdDescr = ProdDescr;
+
+            InitializeComponent();
+
+            FillWindow();
+        }
+        private void FillWindow()
+        {
+            LoadCategories();
+            //LoadName();
+            //LoadDescription();
+
+            texbox_ProdId.Text = ProdId;
+            texbox_ProdName.Text = ProdName;
+            if (ProdCat == null)
+                cb_Categories.SelectedIndex = 0;
+            else
+                cb_Categories.SelectedItem = ProdCat;
+            texbox_ProdDescr.Text = ProdDescr;
+        }
+
+        private void LoadCategories()
+        {
+            if (LoginView.connection.State == ConnectionState.Closed)
+                LoginView.connection.Open();
+
+            string loadCatName = $"SELECT cat_name FROM dbo.prd_categories;";
+            SqlCommand sqlLoadCatName = new SqlCommand(loadCatName, LoginView.connection);
+            SqlDataReader sqlReader = sqlLoadCatName.ExecuteReader();
+
+            while (sqlReader.Read())
+            {
+                cb_Categories.Items.Add(sqlReader["cat_name"].ToString());
+            }
+
+            sqlReader.Close();
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (LoginView.connection.State == ConnectionState.Closed)
+                LoginView.connection.Open();
+
+            // Проверяем есть ли такой товар
+            string getLastProdId = $"SELECT MAX(prd_id) FROM dbo.products;";
+            SqlCommand sqlGetLastProdId = new SqlCommand(getLastProdId, LoginView.connection);
+            int lastProdId = Convert.ToInt32(sqlGetLastProdId.ExecuteScalar());
+
+            // Если нет таких товаров, то записываем новое в базу
+            if (Convert.ToInt32(ProdId) > lastProdId)
+            {
+                string insertProd = @"INSERT INTO dbo.products (prd_name)
+                VALUES (@prd_name);";
+                SqlCommand SqlinsertProd = new SqlCommand(insertProd, LoginView.connection);
+                SqlinsertProd.Parameters.AddWithValue("@prd_name", texbox_ProdName.Text);
+                SqlinsertProd.ExecuteNonQuery();
+
+                string insertDescr = @"INSERT INTO dbo.prd_description (prd_id, cat_id, prd_descr)
+                VALUES (@prd_id, @cat_id, @prd_descr);";
+                SqlCommand SqlInsertDescr = new SqlCommand(insertDescr, LoginView.connection);
+                SqlInsertDescr.Parameters.AddWithValue("@prd_id", Convert.ToInt32(ProdId));
+                SqlInsertDescr.Parameters.AddWithValue("@cat_id", cb_Categories.SelectedIndex + 1);
+                SqlInsertDescr.Parameters.AddWithValue("@prd_descr", texbox_ProdDescr.Text);
+                SqlInsertDescr.ExecuteNonQuery();
+            }
+            // иначе сохраняем текущее
+            else
+            {
+                string updateDescr = "UPDATE dbo.prd_description SET cat_id=@cat_id, prd_descr=@prd_descr WHERE prd_id=@prd_id";
+                SqlCommand SqlupdateDescr = new SqlCommand(updateDescr, LoginView.connection);
+                SqlupdateDescr.Parameters.AddWithValue("@cat_id", cb_Categories.SelectedIndex + 1);
+                SqlupdateDescr.Parameters.AddWithValue("@prd_descr", texbox_ProdDescr.Text);
+                SqlupdateDescr.Parameters.AddWithValue("@prd_id", Convert.ToInt32(ProdId));
+                SqlupdateDescr.ExecuteNonQuery();
+
+                string updateProd = "UPDATE dbo.products SET prd_name=@prd_name WHERE prd_id=@prd_id";
+                SqlCommand SqlupdateProd = new SqlCommand(updateProd, LoginView.connection);
+                SqlupdateProd.Parameters.AddWithValue("@prd_name", texbox_ProdName.Text);
+                SqlupdateProd.Parameters.AddWithValue("@prd_id", Convert.ToInt32(ProdId));
+                SqlupdateProd.ExecuteNonQuery();
+            }
+
+            this.Close();
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
--- Views/ContentView.xaml.cs
+++ Views/ContentView.xaml.cs
@@ -31,6 +31,7 @@ namespace IShop_Management.Views
             dataGrid_Content.DataContext = mergedDT;
         }
 
+        // Наполнение таблицы описаний
         private void FillDataGrid(int? id, string name)
         {
             SqlCommand cmd = new SqlCommand("dbo.ShowContent", LoginView.connection);
@@ -44,9 +45,35 @@ namespace IShop_Management.Views
             adapter.Fill(mergedDT);
         }
 
+        // Редактирование описания
         private void dataGridContent_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
+            DataRowView dataRowView = (DataRowView)dataGrid_Content.SelectedItem;
+            ContentEditView contentEditView = new ContentEditView(
+                dataRowView.Row[0].ToString(),
+                dataRowView.Row[1].ToString(),
+                dataRowView.Row[2].ToString(),
+                dataRowView.Row[3].ToString()
+                );
+            contentEditView.ShowDialog();
+        }
 
+        // Новое описание
+        private void NewProduct_Click(object sender, RoutedEventArgs e)
+        {
+            //string getNewOrdId = $"SELECT MAX(prd_id) FROM dbo.products;";
+            string getNewOrdId = $"SELECT IDENT_CURRENT ('dbo.products');";
+            SqlCommand sqlGetNewProdId = new SqlCommand(getNewOrdId, LoginView.connection);
+            if (LoginView.connection.State == ConnectionState.Closed)
+                LoginView.connection.Open();
+
+            ContentEditView contentEditView = new ContentEditView(
+                (Convert.ToInt32(sqlGetNewProdId.ExecuteScalar()) + 1).ToString(),
+                null,
+                null,
+                null
+                );
+            contentEditView.ShowDialog();
         }
 
         private void SearchById_Click(object sender, RoutedEventArgs e)
@@ -78,6 +105,65 @@ namespace IShop_Management.Views
             dataGrid_Content.DataContext = null;
             FillDataGrid(null, texbox_ProdName.Text);
             dataGrid_Content.DataContext = mergedDT;
+        }
+
+        // Обноевление окна при возврате из редактирования
+        private void WindowActivated(object sender, EventArgs e)
+        {
+            int prodId;
+            if (int.TryParse(texbox_ProdId.Text, out prodId))
+            {
+                prodId = Convert.ToInt32(texbox_ProdId.Text);
+
+                dataGrid_Content.DataContext = null;
+                FillDataGrid(prodId, null);
+                dataGrid_Content.DataContext = mergedDT;
+            }
+            else
+            {
+                dataGrid_Content.DataContext = null;
+                FillDataGrid(null, texbox_ProdName.Text);
+                dataGrid_Content.DataContext = mergedDT;
+            }
+        }
+
+        // Удаление товара
+        private void dataGrid_Content_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            DataGrid dg = sender as DataGrid;
+            if (dg != null)
+            {
+                DataRowView dataRowView = (DataRowView)dg.SelectedItem;
+                int idToDelete = (int)dataRowView.Row[0];
+                if (e.Key == Key.Delete)
+                {
+                    // User is attempting to delete the row
+                    var result = MessageBox.Show(
+                        "Текущий товар будет удален!\n\nПродолжить?",
+                        "Удаление",
+                        MessageBoxButton.YesNo,
+                        MessageBoxImage.Question,
+                        MessageBoxResult.No);
+                    if (result != MessageBoxResult.No)
+                    {
+                        if (LoginView.connection.State == ConnectionState.Closed)
+                            LoginView.connection.Open();
+
+                        string deleteDescr = @"DELETE FROM dbo.prd_description WHERE prd_id=@prd_id;";
+                        SqlCommand SqlDeleteDescr = new SqlCommand(deleteDescr, LoginView.connection);
+                        SqlDeleteDescr.Parameters.AddWithValue("@prd_id", idToDelete);
+                        SqlDeleteDescr.ExecuteNonQuery();
+
+                        string deleteProd = @"DELETE FROM dbo.products WHERE prd_id=@prd_id;";
+                        SqlCommand SqlDeleteProd = new SqlCommand(deleteProd, LoginView.connection);
+                        SqlDeleteProd.Parameters.AddWithValue("@prd_id", idToDelete);
+                        SqlDeleteProd.ExecuteNonQuery();
+
+                        WindowActivated(sender, e);
+                    }
+                    e.Handled = (result == MessageBoxResult.No);
+                }
+            }
         }
     }
 }
